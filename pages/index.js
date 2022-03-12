b4d3a321f9649996bfe1328a9326159688f202de
Warren Fitzhenry <warren@cambridge-intelligence.com>
2022-03-12 13:51:46 +0000

    add typescript'

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export async function getStaticProps() {
+
   const files = fs.readdirSync('posts');
 
   const posts = files.map((fileName) => {
