13931b73743736eb1cb4d9268a0aad024d3c6ad0
Warren Fitzhenry <warren@cambridge-intelligence.com>
2022-03-21 20:13:12 +0000

    add sidebar

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,22 +1,84 @@
-import Link from 'next/link';
+import Link from 'next/link'
+import Sidebar from './sidebar'
+import HomeIcon from '@material-ui/icons/Home'
+import ReceiptIcon from '@material-ui/icons/Receipt'
+import NotificationsIcon from '@material-ui/icons/Notifications'
+import DesktopWindowsIcon from '@material-ui/icons/DesktopWindows'
+import SettingsIcon from '@material-ui/icons/Settings'
+
+function onClick(e, item) {
+  window.alert(JSON.stringify(item, null, 2))
+}
+
+const items = [
+  { name: 'home', label: 'Home', Icon: HomeIcon },
+  {
+    name: 'billing',
+    label: 'Billing',
+    Icon: ReceiptIcon,
+    items: [
+      { name: 'statements', label: 'Statements', onClick },
+      { name: 'reports', label: 'Reports', onClick },
+    ],
+  },
+  'divider',
+  {
+    name: 'settings',
+    label: 'Settings',
+    Icon: SettingsIcon,
+    items: [
+      { name: 'profile', label: 'Profile' },
+      { name: 'insurance', label: 'Insurance', onClick },
+      'divider',
+      {
+        name: 'notifications',
+        label: 'Notifications',
+        Icon: NotificationsIcon,
+        items: [
+          { name: 'email', label: 'Email', onClick },
+          {
+            name: 'desktop',
+            label: 'Desktop',
+            Icon: DesktopWindowsIcon,
+            items: [
+              { name: 'schedule', label: 'Schedule' },
+              { name: 'frequency', label: 'Frequency' },
+            ],
+          },
+          { name: 'sms', label: 'SMS' },
+        ],
+      },
+    ],
+  },
+]
 
 export default function Layout({ children }) {
   return (
-    <div className='flex flex-col min-h-screen'>
-      <header className='bg-fuchsia-100 mb-8 py-4'>
-        <div className='container mx-auto flex justify-center'>
-          <Link href='/'>
+    <div className="flex flex-col min-h-screen">
+      <header className="bg-slate-200 py-4 mb-2">
+        <div className="container mx-auto flex justify-center">
+          <Link href="/">
             <a>🏡</a>
           </Link>
-          <span className='mx-auto'>Welcome to my blog</span>{' '}
+          <span className="mx-auto">Welcome to my blog</span>{' '}
         </div>
       </header>
-      <main className='container mx-auto flex-1'>{children}</main>
-      <footer className='bg-fuchsia-100 mt-8 py-4'>
-        <div className='container mx-auto flex justify-center'>
+      <div className="flex flex-col md:flex-row flex-1">
+        {/* <div className="w-full md:w-60"> */}
+        <Sidebar
+          items={items}
+          depthStep={undefined}
+          depth={undefined}
+          expanded={undefined}
+        />
+        {/* </div> */}
+        <main className="container mx-auto flex-1">{children}</main>
+      </div>
+      <footer className="bg-slate-200 py-4 mt-2">
+        <div className="container mx-auto flex justify-center">
           &copy; 2022 Warren Fitzhenry
         </div>
       </footer>
     </div>
-  );
+  )
 }
---
+++ components/sidebar.tsx
@@ -0,0 +1,94 @@
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import Divider from '@material-ui/core/Divider'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import ExpandLessIcon from '@material-ui/icons/ExpandLess'
+import Collapse from '@material-ui/core/Collapse'
+import { useState, Fragment } from 'react'
+
+function SidebarItem({ depthStep = 10, depth = 0, item, ...rest }) {
+  const [collapsed, setCollapsed] = useState(true)
+  const { label, items, Icon, onClick: onClickProp } = item
+
+  function toggleCollapse() {
+    setCollapsed((prevValue) => !prevValue)
+  }
+
+  function onClick(e) {
+    if (Array.isArray(items)) {
+      toggleCollapse()
+    }
+    if (onClickProp) {
+      onClickProp(e, item)
+    }
+  }
+
+  let expandIcon
+
+  if (Array.isArray(items) && items.length) {
+    expandIcon = !collapsed ? (
+      <ExpandLessIcon className={' font-bold text-green-500'} />
+    ) : (
+      <ExpandMoreIcon className="sidebar-item-expand-arrow" />
+    )
+  }
+
+  return (
+    <>
+      <ListItem className="flex p-2" onClick={onClick} button dense {...rest}>
+        <div
+          style={{ paddingLeft: depth * depthStep }}
+          className="flex w-full items-center"
+        >
+          {Icon && <Icon className="mr-2" fontSize="small" />}
+          <div className="flex w-full">{label}</div>
+        </div>
+        {expandIcon}
+      </ListItem>
+      <Collapse in={!collapsed} timeout="auto" unmountOnExit>
+        {Array.isArray(items) ? (
+          <List disablePadding dense>
+            {items.map((subItem, index) => (
+              <Fragment key={`${subItem.name}${index}`}>
+                {subItem === 'divider' ? (
+                  <Divider className="m-4" />
+                ) : (
+                  <SidebarItem
+                    depth={depth + 1}
+                    depthStep={depthStep}
+                    item={subItem}
+                  />
+                )}
+              </Fragment>
+            ))}
+          </List>
+        ) : null}
+      </Collapse>
+    </>
+  )
+}
+
+function Sidebar({ items, depthStep, depth, expanded }) {
+  return (
+    <div className="w-full md:w-60">
+      <List disablePadding dense>
+        {items.map((sidebarItem, index) => (
+          <Fragment key={`${sidebarItem.name}${index}`}>
+            {sidebarItem === 'divider' ? (
+              <Divider style={{ margin: '6px 0' }} />
+            ) : (
+              <SidebarItem
+                depthStep={depthStep}
+                depth={depth}
+                expanded={expanded}
+                item={sidebarItem}
+              />
+            )}
+          </Fragment>
+        ))}
+      </List>
+    </div>
+  )
+}
+
+export default Sidebar
